0bb18ea71519ce4b696c585fa69f502f9e8e7c4f
Carwi Barrios <carwelsbarrios@gmail.com>
2022-02-10 17:49:52 +0100

    lab

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
-const mongoose = require('mongoose');
+require("dotenv").config();
+
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
+  .connect(
+    `mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PWD}@cluster0.ftety.mongodb.net/carwiDBB?retryWrites=true&w=majority`
+  )
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {})
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+// Recipe.create({
+//   title: "kebab",
+//   level: "Amateur Chef",
+//   ingredients: [
+//     "lamb",
+//     "onion",
+//     "garlic",
+//     "cumin",
+//     "coriander",
+//     "paprika",
+//     "sunflower",
+//   ],
+//   cuiside: "Middle-eastern",
+//   dishType: "main_course",
+//   image:
+//     "https://saborgourmet.com//wp-content/uploads/el-kebab-tipico-de-turquia-istock.jpg",
+//   duration: 20,
+//   creator: "Iskender Efendi",
+// });
+
+Recipe.insertMany(data)
+  .then((allRecipes) =>
+    allRecipes.forEach((recipe) => console.log(recipe.title))
+  )
+  .catch((error) => console.log(error));
+
+Recipe.findOneAndUpdate(
+  { title: "Rigatoni alla Genovese" },
+  { duration: "100" }
+).then((recipe) => console.log(`update ${recipe.title}`));
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+  .then((del) => console.log("Carrot Coke is no longer available"))
+  .catch((error) => console.log(error));
+
+ mongoose.connection.close()
